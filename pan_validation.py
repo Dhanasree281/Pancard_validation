f46b5af1b7100ce867d42144c3c00e4499dfb182
Dhanasree281 <dhanasree836@gmail.com>
2025-08-28 17:37:06 +0530

    inital commit

---
+++ pan_validation.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import re
+df = pd.read_excel('PAN Number Validation Dataset.xlsx')
+#print(df.head(10))
+print('Total records:', len(df))
+total_records=len(df)
+#Data Cleaning
+
+df["Pan_Numbers"]=df['Pan_Numbers'].astype('string').str.strip().str.upper()
+#print(df.head(10))
+#print('\n')
+#print(df[df['Pan_Numbers']==''])
+#print(df[df['Pan_Numbers'].isna()])
+df=df.replace({"Pan_Numbers":''},pd.NA).dropna(subset=['Pan_Numbers'])
+#print(df[df['Pan_Numbers']==''])
+#print(df[df['Pan_Numbers'].isna()])
+
+print('Total records=', len(df))
+print('Unique values=',df["Pan_Numbers"].nunique())
+df=df.drop_duplicates(subset="Pan_Numbers",keep='first')
+print('Total records after removing duplicates:', len(df))
+
+#PAN Validation
+def has_adjacent_repitition(pan):
+    # for i in range(len(pan)-1):
+    #     if pan[i]==pan[i+1]:
+    #         return True 
+    # return False
+    return any(pan[i]==pan[i+1] for i in range(len(pan)-1))
+# print(has_adjacent_repitition('AABCD1234E'))   
+# print(has_adjacent_repitition('AABCD')) 
+# print(has_adjacent_repitition('ABCB'))  
+
+def is_sequential(pan):
+    # for i in range(len(pan)-1):
+    #     if ord(pan[i+1])-ord(pan[i])!=1:
+    #         return False
+    return all(ord(pan[i+1])-ord(pan[i])==1 for i in range(len(pan)-1))
+
+def is_valid_pan(pan):
+    if(len(pan)!=10):
+        return False
+    if not re.match(r'^[A-Z]{5}[0-9]{4}[A-Z]$',pan):
+        return False
+    if has_adjacent_repitition(pan):
+        return False
+    if is_sequential(pan):
+        return False
+    return True
+df["Status"]=df["Pan_Numbers"].apply(lambda x: 'Valid' if is_valid_pan(x) else 'Invalid')
+print(df.head(10))
+valid_cnt=(df["Status"]=='Valid').sum()
+invalid_cnt=(df["Status"]=='Invalid').sum()
+missing_cnt=total_records-(valid_cnt+invalid_cnt)
+
+print('Total records=',total_records)
+print('Valid PAN count:',valid_cnt)
+print('Invalid PAN count:',invalid_cnt)
+print('Missing count:',missing_cnt)
+
+df_summary=pd.DataFrame({"Total Processed records":[total_records],
+                         " Total Valid PAN count":[valid_cnt],
+                         "Total Invalid PAN count":[invalid_cnt],
+                        "Total Missing count":[missing_cnt],
+
+                         })
+print(df_summary.head())
+with pd.ExcelWriter('PAN Validation Report.xlsx') as writer:
+    df.to_excel(writer,sheet_name='PAN Validation Details',index=False)
+    df_summary.to_excel(writer,sheet_name='PAN Validation Summary',index=False)
+    
